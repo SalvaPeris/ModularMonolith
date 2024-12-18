4403778abda4adc760ed313bd68492763c3c154c
SalvaPeris <sperisgimeno@gmail.com>
2024-12-18 08:36:39 +0100

    Wiring up all modules.

--- src/ModularMonolith/Bootstrapper/Api/Program.cs
+++ src/ModularMonolith/Bootstrapper/Api/Program.cs
@@ -1,5 +1,13 @@
+using Basket;
+using Catalog;
+using Order;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderModule(builder.Configuration);
 
 var app = builder.Build();
 
