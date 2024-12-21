ad22fa818f2e0331d1e5224bc189731c93de3146
SalvaPeris <sperisgimeno@gmail.com>
2024-12-21 14:34:28 +0100

    Added CreateProductEndpoint and UpdateProductEndpoint.

--- src/ModularMonolith/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/ModularMonolith/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -7,6 +7,9 @@ global using Microsoft.Extensions.Logging;
 global using FluentValidation;
 global using MediatR;
 global using Mapster;
+global using Carter;
+global using Microsoft.AspNetCore.Http; 
+global using Microsoft.AspNetCore.Routing;
 
 global using Shared.Data;
 global using Shared.DDD;
@@ -19,6 +22,3 @@ global using Catalog.Data;
 global using Catalog.Data.Seed;
 global using Catalog.Products.Dtos;
 global using Catalog.Products.Exceptions;
-
-
-
