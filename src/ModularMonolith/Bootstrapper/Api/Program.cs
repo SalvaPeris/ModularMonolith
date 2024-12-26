5c29f867c89b9481a0dc2e33fc3003729589b513
SalvaPeris <sperisgimeno@gmail.com>
2024-12-26 11:23:49 +0100

    Added Serilog.

--- src/ModularMonolith/Bootstrapper/Api/Program.cs
+++ src/ModularMonolith/Bootstrapper/Api/Program.cs
@@ -1,8 +1,14 @@
 using Carter;
+using Serilog;
 using Shared.Exceptions.Handlers;
 using Shared.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) =>
+{
+    config.ReadFrom.Configuration(context.Configuration);
+});
 
 var catalogAssembly = typeof(CatalogModule).Assembly;
 var basketAssembly = typeof(BasketModule).Assembly;
@@ -24,11 +30,11 @@ builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 var app = builder.Build();
 
 app.MapCarter();
+app.UseSerilogRequestLogging();
+app.UseExceptionHandler(options => { });
 
 app.UseCatalogModule()
    .UseBasketModule()
    .UseOrderModule();
-
-app.UseExceptionHandler(options => { });
 
 app.Run();
