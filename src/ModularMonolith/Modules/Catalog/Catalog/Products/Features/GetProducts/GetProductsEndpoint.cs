0deb631d319a8f0a763e21a07292057b7b0d43e5
SalvaPeris <sperisgimeno@gmail.com>
2024-12-27 10:37:07 +0100

    Erro with PaginationRequest solved in Catalog.

--- src/ModularMonolith/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
+++ src/ModularMonolith/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -8,9 +8,9 @@ namespace Catalog.Products.Features.GetProducts
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products", async (ISender sender) =>
+            app.MapGet("/products", async ([AsParameters] PaginationRequest request, ISender sender) =>
             {
-                var result = await sender.Send(new GetProductsQuery());
+                var result = await sender.Send(new GetProductsQuery(request));
 
                 var response = result.Adapt<GetProductsResponse>();
 
