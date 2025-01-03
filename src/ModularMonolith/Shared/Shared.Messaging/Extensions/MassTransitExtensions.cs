31ff524f542d92a130e31974455f9f5d94f6dd64
Xpander Admin <xpander.admin@adminazurexpander.onmicrosoft.com>
2025-01-03 09:30:57 +0100

    Added Shared.Messaging.

---
+++ src/ModularMonolith/Shared/Shared.Messaging/Extensions/MassTransitExtensions.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Shared.Messaging.Extensions
+{
+    public static class MassTransitExtentions
+    {
+        public static IServiceCollection AddMassTransitWithAssemblies
+            (this IServiceCollection services, IConfiguration configuration, params Assembly[] assemblies)
+        {
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+
+                config.SetInMemorySagaRepositoryProvider();
+
+                config.AddConsumers(assemblies);
+                config.AddSagaStateMachines(assemblies);
+                config.AddSagas(assemblies);
+                config.AddActivities(assemblies);
+
+                config.UsingInMemory((context, configurator) =>
+                {
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
+
+            return services;
+        }
+    }
+}
