3257e4d93eb513889862fe16aaf26a0c0d746afb
Xpander Admin <xpander.admin@adminazurexpander.onmicrosoft.com>
2025-01-08 12:05:42 +0100

    Added Order Dtos.

---
+++ src/ModularMonolith/Modules/Order/Order/Orders/Dtos/OrderItemDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Order.Orders.Dtos
+{
+    public record OrderItemDto(Guid OrderId, Guid ProductId, int Quantity, decimal Price);
+}
---
+++ src/ModularMonolith/Modules/Order/Order/Orders/Dtos/PaymentDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Order.Orders.Dtos
+{
+    public record PaymentDto(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
+}
