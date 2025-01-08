2b65554ea75016aec2e629023033f45bf1d3a099
Xpander Admin <xpander.admin@adminazurexpander.onmicrosoft.com>
2025-01-08 12:47:26 +0100

    Added Order endpoints.

--- src/ModularMonolith/Modules/Order/Order/GlobalUsings.cs
+++ src/ModularMonolith/Modules/Order/Order/GlobalUsings.cs
@@ -1,4 +1,9 @@
-﻿global using Microsoft.EntityFrameworkCore;
+﻿global using Carter;
+global using MediatR;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.EntityFrameworkCore;
 global using FluentValidation;
 global using Mapster;
 
