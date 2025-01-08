d5be09ea5b325dad53ea4e94919bfac5426a73ac
Xpander Admin <xpander.admin@adminazurexpander.onmicrosoft.com>
2025-01-08 12:35:48 +0100

    Added Order Handlers.

--- src/ModularMonolith/Modules/Order/Order/GlobalUsings.cs
+++ src/ModularMonolith/Modules/Order/Order/GlobalUsings.cs
@@ -1,1 +1,10 @@
-﻿global using Shared.DDD;
+﻿global using Microsoft.EntityFrameworkCore;
+global using FluentValidation;
+global using Mapster;
+
+global using Order.Data;
+global using Order.Exceptions;
+global using Order.Orders.Dtos;
+
+global using Shared.DDD;
+global using Shared.Contracts.CQRS;
---
+++ src/ModularMonolith/Modules/Order/Order/Orders/Features/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,32 @@
+﻿using Order.Exceptions;
+
+namespace Order.Orders.Features.DeleteOrder
+{
+    public record DeleteOrderCommand(Guid OrderId)
+        : ICommand<DeleteOrderResult>;
+    public record DeleteOrderResult(bool IsSuccess);
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderName is required");
+        }
+    }
+
+    internal class DeleteOrderHandler(OrderDbContext dbContext)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var order = await dbContext.Orders
+               .FindAsync([command.OrderId], cancellationToken: cancellationToken);
+
+            if (order is null)
+                throw new OrderNotFoundException(command.OrderId);
+
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return new DeleteOrderResult(true);
+        }
+    }
+}
